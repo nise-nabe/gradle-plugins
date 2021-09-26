a8c5d870704b463d40b15f4a3ac09be376c714e4
nise_nabe <nise.nabe@gmail.com>
2021-09-26 17:04:16 +0900

    add extension file

--- subprojects/idea-ext-ext/build.gradle.kts
+++ subprojects/idea-ext-ext/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     `java-gradle-plugin`
 }
 
+dependencies {
+    implementation("gradle.plugin.org.jetbrains.gradle.plugin.idea-ext:gradle-idea-ext:1.+")
+}
+
 gradlePlugin {
     plugins {
         register("idea-ext-ext") {
