6b06fd0318d6b76a113f3933b1da8e5c0cd319b3
nise_nabe <nise.nabe@gmail.com>
2021-09-29 15:00:41 +0900

    add plugin settings

--- subprojects/ci-detect/build.gradle.kts
+++ subprojects/ci-detect/build.gradle.kts
@@ -3,3 +3,24 @@ plugins {
     `java-gradle-plugin`
     id("com.gradle.plugin-publish")
 }
+
+gradlePlugin {
+    plugins {
+        register("ci-detect") {
+            id = "com.nisecoder.ci-detect"
+            implementationClass = "com.nisecoder.gradle.plugin.CiDetectPlugin"
+            description = "detect CI services for each CI build logic"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/nise-nabe/gradle-plugins"
+    vcsUrl = "https://github.com/nise-nabe/gradle-plugins"
+
+    (plugins) {
+        "ci-detect" {
+            tags = listOf("ci")
+        }
+    }
+}
