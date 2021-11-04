dbcde24fedbc2f5c240d2b15108844364d69b010
nise_nabe <nise.nabe@gmail.com>
2021-11-04 17:24:59 +0900

    add test for GitHub actions plugin

---
+++ subprojects/github-actions/src/test/kotlin/com/nisecoder/gradle/plugin/GithubActionsPluginTest.kt
@@ -0,0 +1,50 @@
+package com.nisecoder.gradle.plugin
+
+import com.nisecoder.gradle.test.writeKotlin
+import org.assertj.core.api.Assertions.assertThat
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+import kotlin.test.assertNotNull
+
+class GithubActionsPluginTest {
+    @TempDir
+    private lateinit var testProjectDir: File
+
+    private lateinit var settingsFile: File
+    private lateinit var buildFile: File
+
+
+    @BeforeEach
+    fun setup() {
+        settingsFile = testProjectDir.resolve("settings.gradle.kts")
+        buildFile = testProjectDir.resolve("build.gradle.kts")
+    }
+
+    @Test
+    fun test() {
+        buildFile.writeKotlin("""
+            plugins {
+              id("com.nisecoder.github-actions")
+            }
+            logger.lifecycle(githubActions.actor.get())
+
+        """.trimIndent())
+
+        val buildResult = GradleRunner.create()
+            .withProjectDir(testProjectDir)
+            .withPluginClasspath()
+            .withEnvironment(mapOf("GITHUB_ACTOR" to "mock user name"))
+            .withArguments("help")
+            .build()
+
+        val taskResult = buildResult.task(":help")
+        assertNotNull(taskResult)
+        assertThat(taskResult.outcome).isIn(TaskOutcome.SUCCESS, TaskOutcome.UP_TO_DATE)
+        assertThat(buildResult.output).contains("mock user name")
+
+    }
+}
