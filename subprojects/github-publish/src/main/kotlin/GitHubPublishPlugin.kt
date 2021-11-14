a39a21e16aa2f550ea6a7004422b7e59a3a54f9e
nise_nabe <nise.nabe@gmail.com>
2021-11-14 22:32:24 +0900

    GitHub Packages should publish when `maven-publish` plugin is applied

--- subprojects/github-publish/src/main/kotlin/GitHubPublishPlugin.kt
+++ subprojects/github-publish/src/main/kotlin/GitHubPublishPlugin.kt
@@ -3,20 +3,26 @@ package com.nisecoder.gradle.plugin
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.publish.plugins.PublishingPlugin
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.register
+import org.gradle.kotlin.dsl.withType
 
 class GitHubPublishPlugin: Plugin<Project> {
     override fun apply(project: Project): Unit = project.run {
         plugins.apply(PublishingPlugin::class)
 
-        tasks.register<DefaultTask>("publishToGitHubPackages") {
-            description = "Publishes publications to GitHub Packages"
-            group = PublishingPlugin.PUBLISH_TASK_GROUP
+        // affect to `maven-publish` plugin
+        plugins.withType<MavenPublishPlugin> {
+            tasks.register<DefaultTask>("publishToGitHubPackages") {
+                description = "Publishes publications to GitHub Packages"
+                group = PublishingPlugin.PUBLISH_TASK_GROUP
 
-            dependsOn(tasks.getByName("publish"))
+                dependsOn(tasks.getByName("publish"))
+            }
         }
+
         tasks.register<DefaultTask>("publishToGitHubRelease") {
             description = "Publishes publications to GitHub Release"
             group = PublishingPlugin.PUBLISH_TASK_GROUP
