af9bcb3aa48ccd824d3a312bb91d6d117509fb08
nise_nabe <nise.nabe@gmail.com>
2021-11-14 22:29:10 +0900

    add publish tasks to GitHub

--- subprojects/github-publish/src/main/kotlin/GitHubPublishPlugin.kt
+++ subprojects/github-publish/src/main/kotlin/GitHubPublishPlugin.kt
@@ -1,10 +1,27 @@
 package com.nisecoder.gradle.plugin
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.publish.plugins.PublishingPlugin
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.register
 
 class GitHubPublishPlugin: Plugin<Project> {
     override fun apply(project: Project): Unit = project.run {
-        println("Hello, GitHubPublishPlugin!")
+        plugins.apply(PublishingPlugin::class)
+
+        tasks.register<DefaultTask>("publishToGitHubPackages") {
+            description = "Publishes publications to GitHub Packages"
+            group = PublishingPlugin.PUBLISH_TASK_GROUP
+
+            dependsOn(tasks.getByName("publish"))
+        }
+        tasks.register<DefaultTask>("publishToGitHubRelease") {
+            description = "Publishes publications to GitHub Release"
+            group = PublishingPlugin.PUBLISH_TASK_GROUP
+
+            dependsOn(tasks.getByName("publish"))
+        }
     }
 }
