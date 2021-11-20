ba24461805da672aaebc588adfc46d1e8507a1b4
nise_nabe <nise.nabe@gmail.com>
2021-11-20 21:30:18 +0900

    rm maven-publish support

--- subprojects/github-publish/src/main/kotlin/GitHubPublishPlugin.kt
+++ subprojects/github-publish/src/main/kotlin/GitHubPublishPlugin.kt
@@ -3,13 +3,12 @@ package com.nisecoder.gradle.plugin
 import com.nisecoder.gradle.plugin.github.publish.GitHubPublication
 import com.nisecoder.gradle.plugin.github.publish.GitHubPublicationFactory
 import com.nisecoder.gradle.plugin.github.task.PublishToGitHubRelease
+import org.gradle.api.NamedDomainObjectCollection
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
-import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
-import org.gradle.api.publish.maven.tasks.PublishToMavenRepository
 import org.gradle.api.publish.plugins.PublishingPlugin
+import org.gradle.api.tasks.TaskContainer
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.register
@@ -23,37 +22,31 @@ class GitHubPublishPlugin: Plugin<Project> {
         configure<PublishingExtension> {
             publications.registerFactory(GitHubPublication::class.java, GitHubPublicationFactory(objects))
             val gitHubPublications = publications.withType<GitHubPublication>()
+            val gitHubReleaseRepositories = repositories.withType<GitHubReleaseRepository>()
 
             gitHubPublications.all {
-                val targetPublication = this
-                tasks.register<PublishToGitHubRelease>("publishToGitHubRelease") {
-                    description = "Publishes publications to GitHub Release"
-                    group = PublishingPlugin.PUBLISH_TASK_GROUP
-
-                    publication = targetPublication
-
-                    dependsOn(tasks.getByName("publish"))
-                }
+                tasks.registerGitHubReleaseTasks(this, gitHubReleaseRepositories)
             }
         }
+    }
 
-        // affect to `maven-publish` plugin
-        plugins.withType<MavenPublishPlugin> {
-            configure<PublishingExtension> {
-                val mavenPublications = publications.withType<MavenPublication>()
+    private fun TaskContainer.registerGitHubReleaseTasks(
+        targetPublication: GitHubPublication,
+        gitHubReleaseRepository: NamedDomainObjectCollection<GitHubReleaseRepository>,
+    ) {
+        gitHubReleaseRepository.all {
+            val repository = this
+            register<PublishToGitHubRelease>("publishToGitHubRelease") {
+                description = "Publishes publications to GitHub Release"
+                group = PublishingPlugin.PUBLISH_TASK_GROUP
 
-                mavenPublications.all {
-                    val mavenPublication = this
-                    tasks.register<PublishToMavenRepository>("publishToGitHubPackages") {
-                        description = "Publishes publications to GitHub Packages"
-                        group = PublishingPlugin.PUBLISH_TASK_GROUP
+                publication = targetPublication
+                githubRepository.set(repository.url)
 
-                        publication = mavenPublication
-
-                        dependsOn(tasks.getByName("publish"))
-                    }
-                }
+                dependsOn(named("publish"))
             }
+
         }
+
     }
 }
---
+++ subprojects/github-publish/src/main/kotlin/GitHubReleaseRepository.kt
@@ -0,0 +1,20 @@
+package com.nisecoder.gradle.plugin
+
+import org.gradle.api.Action
+import org.gradle.api.artifacts.repositories.ArtifactRepository
+import org.gradle.api.artifacts.repositories.RepositoryContentDescriptor
+import org.gradle.api.artifacts.repositories.UrlArtifactRepository
+
+class GitHubReleaseRepository(
+    private var name: String
+): ArtifactRepository {
+    var url: String = ""
+    private val urlArtifactRepository: UrlArtifactRepository? = null
+
+    override fun getName() = name
+    override fun setName(name: String) { this.name = name }
+
+    override fun content(configureAction: Action<in RepositoryContentDescriptor>) {
+        TODO("Not yet implemented")
+    }
+}
