27191c55a362567ba81404736dca92965dad2780
nise_nabe <nise.nabe@gmail.com>
2021-11-20 22:11:21 +0900

    url should be set after

--- subprojects/github-publish/src/main/kotlin/github/publish/GitHubReleaseRepository.kt
+++ subprojects/github-publish/src/main/kotlin/github/publish/GitHubReleaseRepository.kt
@@ -3,13 +3,11 @@ package com.nisecoder.gradle.plugin.github.publish
 import org.gradle.api.Action
 import org.gradle.api.artifacts.repositories.ArtifactRepository
 import org.gradle.api.artifacts.repositories.RepositoryContentDescriptor
-import org.gradle.api.artifacts.repositories.UrlArtifactRepository
 
 class GitHubReleaseRepository(
     private var name: String
 ): ArtifactRepository {
-    var url: String = ""
-    private val urlArtifactRepository: UrlArtifactRepository? = null
+    lateinit var url: String
 
     override fun getName() = name
     override fun setName(name: String) { this.name = name }
