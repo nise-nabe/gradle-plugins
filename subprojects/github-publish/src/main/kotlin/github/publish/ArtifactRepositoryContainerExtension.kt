56e924b7f7d1a204dd460e8cad7741e04932327f
nise_nabe <nise.nabe@gmail.com>
2021-11-21 09:27:23 +0900

    fix interface to be same with other specific repositories

--- subprojects/github-publish/src/main/kotlin/github/publish/ArtifactRepositoryContainerExtension.kt
+++ subprojects/github-publish/src/main/kotlin/github/publish/ArtifactRepositoryContainerExtension.kt
@@ -1,10 +1,12 @@
 package com.nisecoder.gradle.plugin.github.publish
 
 import org.gradle.api.Action
-import org.gradle.api.artifacts.ArtifactRepositoryContainer
+import org.gradle.api.artifacts.dsl.RepositoryHandler
 
-fun ArtifactRepositoryContainer.gitHubRelease(action: Action<GitHubReleaseRepository>) {
-    add(GitHubReleaseRepository("gitHubRelease").apply {
+fun RepositoryHandler.gitHubRelease(action: Action<GitHubReleaseRepository>): GitHubReleaseRepository {
+    val repository = GitHubReleaseRepository("gitHubRelease").apply {
         action.execute(this)
-    })
+    }
+    add(repository)
+    return repository
 }
