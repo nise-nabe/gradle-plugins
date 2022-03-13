6599aa4e967bdf7d998f4730e992f842490861b8
nise_nabe <nise.nabe@gmail.com>
2022-03-13 15:12:34 +0900

    fix to use gradle.kts language

--- subprojects/lib-gradle-test/src/main/kotlin/test/FileExtensions.kt
+++ subprojects/lib-gradle-test/src/main/kotlin/test/FileExtensions.kt
@@ -6,7 +6,7 @@ import org.intellij.lang.annotations.Language
 import java.io.File
 
 
-fun File.writeKotlin(@Language("kotlin") src: String) {
+fun File.writeKotlin(@Language("gradle.kts") src: String) {
     writeText(src)
 }
 
