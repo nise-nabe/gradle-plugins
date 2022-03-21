1eb65cbb3c25ba1d05005c543f63519ba0249d5b
nise_nabe <nise.nabe@gmail.com>
2022-03-21 13:19:11 +0900

    extend exec for node task

--- subprojects/node-plugin/src/main/kotlin/node/NodeTask.kt
+++ subprojects/node-plugin/src/main/kotlin/node/NodeTask.kt
@@ -1,32 +1,28 @@
 package com.nisecoder.gradle.plugin.node
 
-import org.gradle.api.DefaultTask
 import org.gradle.api.internal.file.FileOperations
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
-import org.gradle.process.ExecOperations
 import javax.inject.Inject
 
-abstract class NodeTask: DefaultTask() {
+abstract class NodeTask: Exec() {
     @get:Internal
     abstract val nodeProvisioningService: Property<NodeProvisioningService>
 
     @get:Inject
     abstract val fileOperations: FileOperations
 
-    @get:Inject
-    abstract val execOperations: ExecOperations
-
     @get:Internal
     abstract val nodeVersion: Property<String>
 
     @TaskAction
-    fun exec() {
+    override fun exec() {
         val node = nodeProvisioningService.get().provision(fileOperations, nodeVersion.get())
 
-        execOperations.exec {
-            commandLine(node.node, "-v")
-        }
+        commandLine(node.node, "-v")
+
+        super.exec()
     }
 }
