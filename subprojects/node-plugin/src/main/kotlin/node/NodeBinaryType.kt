c74158048fedaa126dc4a8dc123b32c5bf1d3e4d
nise_nabe <nise.nabe@gmail.com>
2022-03-22 10:25:49 +0900

    linux binaries supports gz archive

--- subprojects/node-plugin/src/main/kotlin/node/NodeBinaryType.kt
+++ subprojects/node-plugin/src/main/kotlin/node/NodeBinaryType.kt
@@ -5,7 +5,7 @@ enum class NodeBinaryType(val osName: String, val arch: String, val ext: String)
     Win64("win", "x64", "zip"),
     Mac64("darwin", "x64", "tar.gz"),
     MacArm64("darwin", "arm64", "tar.gz"),
-    Linux64("linux", "x64", "tar.xz"),
-    LinuxArmv7("linux", "armv7l", "tar.xz"),
-    LinuxArmv8("linux", "armv64", "tar.xz"),
+    Linux64("linux", "x64", "tar.gz"),
+    LinuxArmv7("linux", "armv7l", "tar.gz"),
+    LinuxArmv8("linux", "armv64", "tar.gz"),
 }
