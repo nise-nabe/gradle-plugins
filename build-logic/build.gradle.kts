3a7c548de513e14b8f844b26be0e1af5c3272f5a
nise_nabe <nise.nabe@gmail.com>
2024-02-24 12:24:24 +0900

    try to build for each java version and use gradle runtime version on plugins build to check build

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -6,9 +6,3 @@ dependencies {
     implementation("gradle.plugin.org.jetbrains.gradle.plugin.idea-ext:gradle-idea-ext:${libs.versions.idea.ext.get()}")
     implementation("com.gradle.publish:plugin-publish-plugin:1.2.1")
 }
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(11)
-    }
-}
