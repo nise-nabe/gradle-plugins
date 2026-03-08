b6ecd416797c30b5e7c7d37d4aba4dc1563bfcd3
nise_nabe <nise.nabe@gmail.com>
2026-03-08 10:32:09 +0900

    remove unused `import java.net.URL` from `PluginsXmlTest`

--- subprojects/publish-intellij-plugin/src/test/kotlin/model/PluginsXmlTest.kt
+++ subprojects/publish-intellij-plugin/src/test/kotlin/model/PluginsXmlTest.kt
@@ -7,7 +7,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.net.URI
-import java.net.URL
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
